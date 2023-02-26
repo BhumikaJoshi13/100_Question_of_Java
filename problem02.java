529a47e536c42f4b67e9ed3f666a8ec8551b2a82
Bhumika13 <109601177+BhumikaJoshi13@users.noreply.github.com>
2023-02-26 09:43:11 +0530

    Add files via upload

---
+++ problem02.java
@@ -0,0 +1,10 @@
+//Print Integer in Java 
+public class problem02 {
+    public static void main(String[] args) {
+        int c;
+        for( c=1;c<=10;c++)
+        {
+            System.out.println(c);
+        }
+    }
+}
