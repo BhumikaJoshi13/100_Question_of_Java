bc3ebfdb6ddfcaf52c0ac01fe52984d262ce8e86
Bhumika13 <109601177+BhumikaJoshi13@users.noreply.github.com>
2023-03-05 23:33:05 +0530

    Add files via upload

---
+++ problem_16.java
@@ -0,0 +1,10 @@
+//WAP Using for loop 
+public class problem_16 {
+    public static void main(String[] args) {
+        int c;
+        for( c=1 ; c<=10; c++)
+        {
+            System.out.println(c);
+        }
+    }
+}
