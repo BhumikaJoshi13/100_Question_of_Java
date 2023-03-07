db1e91f30a6f212b772df82f8b3db04bec843875
Bhumika13 <109601177+BhumikaJoshi13@users.noreply.github.com>
2023-03-07 20:26:05 +0530

    Add files via upload

---
+++ problem_22.java
@@ -0,0 +1,10 @@
+//Print all Alphabet using For loop
+public class problem_22 {
+    public static void main(String[] args) {
+        char ch ;
+       for(ch='a';ch<='z';ch++)
+       {
+        System.out.print(ch);
+       }
+    }
+}
---
+++ problem_23.java
@@ -0,0 +1,9 @@
+public class problem_23 {
+    public static void main(String[] args) {
+        int prime[] = { 2,3,5,7,11};
+        for( int t:prime)
+        {
+            System.out.println(t);
+        }
+    }
+}
